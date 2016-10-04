95feea97f7ea71e4b65497954867b14fdff4c64d
Lionel T <elrumordelaluz@hotmail.com>
2016-10-04 12:53:48 +0200

    Full Demo

--- docs/entry.js
+++ docs/entry.js
@@ -12,9 +12,29 @@ import {
   ShakeCrazy,
 } from '../src'
 
+const shakes = [
+  ShakeLittle,
+  ShakeSlow,
+  ShakeHard,
+  ShakeHorizontal,
+  ShakeVertical,
+  ShakeRotate,
+  ShakeCrazy,
+]
+
+const emojee = [
+  '😬',
+  '🙄',
+  '😎',
+  '🚙',
+  '👆',
+  '🔔',
+  '😹',
+]
+
 const styles = StyleSheet.create({
   wrapper: {
-    padding: '2em 4em',
+    padding: '2em 10em 4em',
     backgroundColor: '#2ab8ac',
     color: 'white',
     minHeight: '100vh',
@@ -23,10 +43,11 @@ const styles = StyleSheet.create({
     fontFamily: 'Dancing Script, cursive',
     fontSize: '4em',
     marginBottom: 0,
+    marginTop: 0,
   },
   link: {
     textDecoration: 'none',
-    color: '#272822',
+    color: '#136760',
   },
   code: {
     display: 'block',
@@ -40,11 +61,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(240,240,240,.25)',
     border:' 1px solid rgba(240,240,240,.25)',
     borderRadius: '4px',
+    overflow: 'auto',
   },
   
   section: {
     display: 'flex',
-    alignItems: 'flex-start',
+    alignItems: 'center',
+    flexWrap: 'wrap',
   },
   side: {
     flex: '0 0 12em',
@@ -54,7 +77,6 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    minHeight: '250px',
   },
   
   range: {
@@ -78,7 +100,18 @@ const styles = StyleSheet.create({
   },
   
   checkbox: {
-    fontSize: '3em'
+    fontSize: '3em',
+  },
+  
+  emoji: {
+    fontSize: '3em',
+  },
+  
+  footer: {
+    width: '100%',
+    textAlign: 'center',
+    fontSize: '.85em',
+    marginTop: '3em',
   },
 })
 
@@ -120,7 +153,7 @@ const Check = ({ checked = false, label, onChange }) => {
 
 class Customizer extends Component {
   constructor (props) {
-    super(props);
+    super(props)
     
     this.state = {
       h: 5,
@@ -154,129 +187,173 @@ class Customizer extends Component {
   
   render () {
     const { h, v, r, dur, int, max, fixed, fixedStop, freez } = this.state
-    const changeH = e => this.handleChangeRange(e, 'h');
-    const changeV = e => this.handleChangeRange(e, 'v');
-    const changeR = e => this.handleChangeRange(e, 'r');
-    const changeDur = e => this.handleChangeRange(e, 'dur');
-    const changeInt = e => this.handleChangeRange(e, 'int');
-    const changeMax = e => this.handleChangeRange(e, 'max');
-    const toggleFixed = e => this.handleChangeCheckbox(e, 'fixed');
-    const toggleFixedStop = e => this.handleChangeCheckbox(e, 'fixedStop');
-    const toggleFreez = e => this.handleChangeCheckbox(e, 'freez');
+    const changeH = e => this.handleChangeRange(e, 'h')
+    const changeV = e => this.handleChangeRange(e, 'v')
+    const changeR = e => this.handleChangeRange(e, 'r')
+    const changeDur = e => this.handleChangeRange(e, 'dur')
+    const changeInt = e => this.handleChangeRange(e, 'int')
+    const changeMax = e => {
+      this.handleChangeRange(e, 'max')
+      if (int > max) {
+        this.setState({
+          int: max
+        })
+      }
+    }
+    const toggleFixed = e => this.handleChangeCheckbox(e, 'fixed')
+    const toggleFixedStop = e => this.handleChangeCheckbox(e, 'fixedStop')
+    const toggleFreez = e => this.handleChangeCheckbox(e, 'freez')
     
     return (
-      <section className={css(styles.section)}>
-        <main className={css(styles.main)}>
-          <Shake 
-            fixed={fixed}
-            fixedStop={fixedStop}
-            freez={freez}
-            h={h}
-            v={v}
-            r={r}
-            dur={dur}
-            int={int}
-            max={max}>
-            <h1>&lt;Shake /&gt;</h1>
-          </Shake>
-        </main>
-        
-        <aside className={css(styles.side)}>
-          <Range label={`Horizontal ${h}`} value={h} onChange={changeH} />
-          <Range label={`Vertical ${v}`} value={v} onChange={changeV} />
-          <Range 
-            label={`Rotate ${r}`} 
-            max={360}
-            value={r} 
-            onChange={changeR} />  
-          <Range 
-            label={`Duration ${dur}`} 
-            value={dur} 
-            min={10}
-            max={1000}
-            step={10}
-            onChange={changeDur} />
-          <Range 
-            label={`Interval ${int}`} 
-            value={int} 
-            min={0.5}
-            max={50}
-            step={0.2}
-            onChange={changeInt} />
-          <Range 
-            label={`Max ${max}`} 
-            value={max} 
-            min={1}
-            max={100}
-            step={1}
-            onChange={changeMax} />
-          <Check 
-            label="freez" 
-            onChange={toggleFreez}
-            checked={freez} />
-          <Check 
-            label="fixed" 
-            onChange={toggleFixed}
-            checked={fixed} />
-          { fixed && (
+      <section>
+        <section className={css(styles.section)}>
+          <main className={css(styles.main)}>
+            <Shake 
+              fixed={fixed}
+              fixedStop={fixedStop}
+              freez={freez}
+              h={h}
+              v={v}
+              r={r}
+              dur={dur}
+              int={int}
+              max={max}>
+              <h1 className={css(styles.title)}>&lt;Shake /&gt;</h1>
+            </Shake>
+          </main>
+          
+          <aside className={css(styles.side)}>
+            <Range label={`Horizontal ${h}`} value={h} onChange={changeH} />
+            <Range label={`Vertical ${v}`} value={v} onChange={changeV} />
+            <Range 
+              label={`Rotate ${r}`} 
+              max={360}
+              value={r} 
+              onChange={changeR} />  
+            <Range 
+              label={`Duration ${dur}`} 
+              value={dur} 
+              min={10}
+              max={1000}
+              step={10}
+              onChange={changeDur} />
+            <Range 
+              label={`Interval ${int}`} 
+              value={int} 
+              min={0.5}
+              max={max}
+              step={0.1}
+              onChange={changeInt} />
+            <Range 
+              label={`Max ${max}`} 
+              value={max} 
+              min={1}
+              max={100}
+              step={1}
+              onChange={changeMax} />
             <Check 
-              label="fixedStop" 
-              onChange={toggleFixedStop}
-              checked={fixedStop} />
-          )}
-        </aside>
-        
+              label="freez" 
+              onChange={toggleFreez}
+              checked={freez} />
+            <Check 
+              label="fixed" 
+              onChange={toggleFixed}
+              checked={fixed} />
+            { fixed && (
+              <Check 
+                label="fixedStop" 
+                onChange={toggleFixedStop}
+                checked={fixedStop} />
+            )}
+          </aside>
+          
+        </section>
+        <pre className={css(styles.code)}>
+          <code>
+{`import React, { Component } from 'react'
+import Shake from 'reshake'
+
+class App extends Component {
+  render () {
+    return (
+      <Shake 
+        h={${h}}
+        v={${v}}
+        r={${r}}
+        dur={${dur}}
+        int={${int}}
+        max={${max}}
+        fixed={${fixed}}
+        fixedStop={${fixedStop}}
+        freez={${freez}}>
+        <h1>&lt;Shake /&gt;</h1>
+      </Shake>
+    )
+  }
+}`}
+          </code>
+        </pre>
       </section>
-    );
+    )
   }
 }
+
 
 class App extends Component {
   render() {
     return (
       <div className={css(styles.wrapper)}>
+        
         <h1 className={css(styles.title)}>&lt;Reshake/&gt;</h1>
         <p>
           <Shake elem='span'>
             <a 
               className={css(styles.link)} 
               href="http://elrumordelaluz.github.io/csshake/">CSShake</a>
-          </Shake> as a React functional Component
+          </Shake> as a React functional Component <a 
+            className={css(styles.link)} 
+            href="https://github.com/elrumordelaluz/reshake">[GitHub]</a>
         </p>
+        
         <pre className={css(styles.code)}>
           <code>
             npm i --save reshake
           </code>
         </pre>
+        
         <Customizer />
-        { /*
-        <Shake fixed
-          className="my-custom-class"
-          style={{ color: 'deepskyblue' }}>
-          &lt;Shake /&gt;
-        </Shake>
-        <Shake fixed>
-          &lt;Shake fixed /&gt;
-        </Shake>
-        <Shake freez style={{ 
-            textDecoration: 'line-through',
-            fontWeight: '100',
-            margin: '1em'
-          }}>
-        	&lt;Shake freez /&gt;
-        </Shake>
-        <Shake fixed freez>
-        	&lt;Shake fixed freez /&gt;
-        </Shake>
-        <Shake fixed fixedStop style={{ 
-            letterSpacing: '2px',
-            margin: '1em'
-          }}>
-        	&lt;Shake fixed fixedStop /&gt;
-        </Shake>
-         */ }
+        
+        <h1 className={css(styles.title)}>More...</h1>
+        <p>Separated Components for each animation type</p>
+        <ul>
+          { shakes.map((s,i) => {
+            const { name } = s
+            const Elem = s
+            return (
+              <li key={name} className={css(styles.section)}>
+                <aside className={css(styles.side)}>
+                  <pre className={css(styles.code)}>
+                    <code>
+                      {`<${name} />`}
+                    </code>
+                  </pre>
+                </aside>
+                <main className={css(styles.main)}>
+                  <Elem 
+                    orig={name === 'ShakeRotate' ? 'top center' : 'center center'}
+                    className={css(styles.emoji)}>
+                    { emojee[i] }
+                  </Elem>
+                </main>
+              </li>
+            )})
+          }
+        </ul>
+        <footer className={css(styles.footer)}>
+          Made with <ShakeSlow fixed>♡</ShakeSlow> by <a className={css(styles.link)} 
+            href="https://twitter.com/elrumordelaluz">@elrumordelaluz</a> using React.</footer>
       </div>
-    );
+    )
   }
 }
 
